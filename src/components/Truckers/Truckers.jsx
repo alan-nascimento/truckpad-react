d7dd6a75092a6a2f8567e5ee216b622b02f8d846
Alan Nascimento <alan.silva.n@hotmail.com>
2019-07-14 21:12:44 -0300

    adding details in the cards

--- src/components/Truckers/Truckers.jsx
+++ src/components/Truckers/Truckers.jsx
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import Fab from '@material-ui/core/Fab';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Edit } from '@material-ui/icons/index';
 
 import './Truckers.scss';
 
@@ -19,7 +24,36 @@ export default function Truckers() {
       {truckers.map(trucker => (
         <article key={trucker.id}>
           <strong>{trucker.name}</strong>
-          <p>{trucker.birthDate}</p>
+          <p>
+            Telefone:
+            <span>{trucker.phone}</span>
+          </p>
+          <p>
+            Data de nascimento:
+            <span>{trucker.birthDate}</span>
+          </p>
+          <p>
+            CNH:
+            <span>{trucker.documents[0].number}</span>
+          </p>
+          <p>
+            Categoria:
+            <span>{trucker.documents[0].category}</span>
+          </p>
+          <p>
+            CPF:
+            <span>{trucker.documents[1].number}</span>
+          </p>
+          <Tooltip title="Delete">
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Edit" aria-label="Edit">
+            <Fab className="edit-icon">
+              <Edit />
+            </Fab>
+          </Tooltip>
         </article>
       ))}
     </div>
