c02251693c498428aa71a001f7ffc66a78ea7749
Alan Nascimento <alan.silva.n@hotmail.com>
2019-07-21 19:02:15 -0300

    splitting truckers component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import Header from './components/Header/Header';
 import Truckers from './components/Truckers/Truckers';
+import AddTruckerForm from './components/AddTruckerForm/AddTruckerForm';
 
 export default function App() {
+  const [truckers, setTruckers] = useState([]);
+
+  async function fetchData() {
+    const response = await fetch('http://localhost:8080/truckers');
+    const data = await response.json();
+    setTruckers(data.reverse());
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const addTrucker = async (trucker) => {
+    await fetch('http://localhost:8080/truckers', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(trucker),
+    })
+      .then(res => res.json())
+      .catch(err => console.log(err));
+
+    await fetchData();
+  };
+
+  const deleteTrucker = async (id) => {
+    fetch(`http://localhost:8080/truckers/${id}`, { method: 'DELETE' }).catch(err => console.log(err));
+    setTruckers(truckers.filter(trucker => trucker._id !== id));
+  };
+
   return (
     <div className="App">
       <Header />
-      <Truckers />
+      <AddTruckerForm addTrucker={addTrucker} />
+      <Truckers truckers={truckers} deleteTrucker={deleteTrucker} />
     </div>
   );
 }
--- src/components/Truckers/Truckers.jsx
+++ src/components/Truckers/Truckers.jsx
@@ -1,43 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Button } from '@material-ui/core';
 import Delete from '@material-ui/icons/Delete';
-import AddTruckerForm from '../AddTruckerForm/AddTruckerForm';
 
 import './Truckers.scss';
 
-export default function Truckers() {
-  const [truckers, setTruckers] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('http://localhost:8080/truckers');
-      const data = await response.json();
-      setTruckers(data.reverse());
-    }
-    fetchData();
-  }, []);
-
-  const addTrucker = async (trucker) => {
-    await fetch('http://localhost:8080/truckers', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(trucker),
-    })
-      .then(res => res.json())
-      .catch(err => console.log(err));
-
-    setTruckers([trucker, ...truckers]);
-  };
-
-  const deleteTrucker = async (id) => {
-    await fetch(`http://localhost:8080/truckers/${id}`, { method: 'DELETE' }).catch(err => console.log(err));
-    setTruckers(truckers.filter(trucker => trucker._id !== id));
-  };
-
+export default function Truckers(props) {
   return (
     <div className="trucker-list">
-      <AddTruckerForm addTrucker={addTrucker} />
-      {truckers.map(trucker => (
+      {props.truckers.map(trucker => (
         <article key={trucker._id}>
           <strong>{trucker.name}</strong>
           <p>
@@ -60,7 +30,7 @@ export default function Truckers() {
             CPF:
             <span>{trucker.documents[1].number}</span>
           </p>
-          <Button type="button" onClick={() => deleteTrucker(trucker._id)}>
+          <Button type="button" onClick={() => props.deleteTrucker(trucker._id)}>
             <Delete />
           </Button>
         </article>
